0eee72c450ff1ff3729d897c950d0c33aafbcb3f
Denis Goyes <denisgoyes14@gmail.com>
2026-03-19 14:11:56 -0500

    feat: add card and toaster components, update login page with Google login functionality, and enhance layout styles

--- src/pages/auth/login.page.tsx
+++ src/pages/auth/login.page.tsx
@@ -1,14 +1,37 @@
+import { Button } from "@/components/ui/button"
 import { useAuthActions } from "../../hooks/use-auth-actions"
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
+import { toast } from "sonner"
 
 const LoginPage = () => {
 
   const {loginWithGoogle} = useAuthActions()
+    
+    const handleLoginWithGoogle = async () => {
+    //toast.error("Login Falled")
+    const result = await loginWithGoogle();
+    if (result.success) {
+      console.log("Login successful");
+    } else {
+      console.error("Login failed:", result.error);
+      toast.error("Login Falled")
+    }
+  };
+
 
   return (
-    <div>
-      <h1>Loadign</h1>
-      <button onClick={loginWithGoogle}>Iniciar Con Google</button>  
-    </div>
+    <Card>
+      <CardHeader>
+        <CardTitle>Login</CardTitle>
+        <CardDescription>Login to you accont using email and password or with Google</CardDescription>
+        <CardContent>
+          .....
+        </CardContent>
+        <CardFooter>
+          <Button onClick={handleLoginWithGoogle} className="w-full">Login with Google</Button>
+        </CardFooter>
+      </CardHeader>
+    </Card>
   )
 }
 
